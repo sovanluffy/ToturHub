30cd8e2df6734710d31b611c372d0070801a14ee
thounsova <thoun.sovannareach@institute.pse.ngo>
2026-04-06 09:26:33 +0700

    add new code

--- src/main/java/com/rental_api/ServiceBooking/config/SecurityConfiguration.java
+++ src/main/java/com/rental_api/ServiceBooking/config/SecurityConfiguration.java
@@ -140,7 +140,7 @@ public class SecurityConfiguration {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-
+//code
         return source;
     }
 }
